e54ad801a927abd612238147f8a411f53bc83b6a
QuiteTheChoi <jcaverly92@gmail.com>
2016-02-19 19:30:20 -0500

    COMMIT GET

--- Server/src/get.java
+++ Server/src/get.java
@@ -3,7 +3,7 @@ import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
-public class ClientG {
+public class get {
 	public static void main(String args[]) throws Exception {
 		String line;	// user input
 		BufferedReader userdata = new BufferedReader(new InputStreamReader(System.in));
