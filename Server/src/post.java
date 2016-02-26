a687a0219c721fca520647266a1fcf1df092d853
mprigozhiy <mprigozhiy@gmail.com>
2016-02-25 19:22:17 -0500

    Updates

--- Server/src/post.java
+++ Server/src/post.java
@@ -5,7 +5,7 @@ import java.net.Socket;
 
 public class post {
 	public static void main(String args[]) throws Exception {
-		String group;
+		String group = null;
 		
 		if(args.length == 1){
 			//Socket sock = new Socket("localhost", 12345);
@@ -27,17 +27,30 @@ public class post {
 		
 		
 		String line;	// user input
-		BufferedReader userdata = new BufferedReader(new InputStreamReader(System.in));
-/*	
+		
+	
 		Socket sock = new Socket("localhost", 12345);	// connect to localhost port 12345
 		DataOutputStream toServer = new DataOutputStream(sock.getOutputStream());
 		BufferedReader fromServer = new BufferedReader(new InputStreamReader(sock.getInputStream()));
 	
-		line = "hello";		// read a line from the user
+		line = "post " + group;		// read a line from the user
 		toServer.writeBytes(line + '\n');	// send the line to the server
 		String result = fromServer.readLine();	// read a one-line result
+		
+		if(result.equals("ok\n")){
+			line = System.getProperty("user.name");
+			System.out.println(System.getProperty("user.name"));
+			toServer.writeBytes(line + '\n');
+		}
+		
+		result = fromServer.readLine();
+		if(result.equals("ok\n")){
+			BufferedReader userdata = new BufferedReader(new InputStreamReader(System.in));
+			toServer.writeBytes(userdata + "\n");
+		}
+		
 		System.out.println(result);		// print it
 		sock.close();				// and we're done
-	*/
+	
 	}
 }
