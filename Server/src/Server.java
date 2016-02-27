0c9d157b3c0d71570c5c518986f84f744b86a9c8
Jon Getahun <Jon@nbp-132-105.nbp.ruw.rutgers.edu>
2016-02-26 21:48:25 -0500

    Updated file(s). - Get

--- Server/src/Server.java
+++ Server/src/Server.java
@@ -13,6 +13,7 @@ import java.util.Map;
 
 public class Server implements Runnable {
 
+	//HashMap containing array lists of messages (groups), with group names being the keys
 	static Map<String,List<message>> serv = new HashMap<String,List<message>>();
 
 	public static void main(String[] args) throws IOException {
@@ -20,42 +21,41 @@ public class Server implements Runnable {
 		ServerSocket svc = null;
 
 		try{
-			if (args.length != 0) {
-				svc = new ServerSocket(Integer.parseInt(args[1]), 5);	
+			if (args.length != 0) { //If there is an argument
+				svc = new ServerSocket(Integer.parseInt(args[1]), 5); //Listen for the inputted port number
 			}
 			else {
-				svc = new ServerSocket(12345, 5);	// listen on port 12345
+				svc = new ServerSocket(12345, 5); //Listen for port 12345
 			}
-		} catch(NumberFormatException e){
+		} catch(NumberFormatException e){ //If the argument cannot be parsed to an integer
 			System.out.println("Invalid Port Number.");
-			System.exit(1);
+			System.exit(1); //Exit the program
 		}
 
 		while(true){
-			final Socket conn = svc.accept();	// get a connection
-			Thread t = new Thread(new Runnable(){
+			final Socket conn = svc.accept(); //Waits for a connection
+			Thread t = new Thread(new Runnable(){ //Creates a new thread
 
-				Socket socket = conn;
+				Socket socket = conn; //Creates a socket
 				@Override
 				public void run() {
 
-					// get the input/output streams for the socket
+					// Get the input/output streams for the socket
 					BufferedReader fromClient = null; 
 					DataOutputStream toClient = null;
 
 					try {
-						fromClient = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-						toClient =  new DataOutputStream(conn.getOutputStream());
+						fromClient = new BufferedReader(new InputStreamReader(conn.getInputStream())); //Messages from the client
+						toClient =  new DataOutputStream(conn.getOutputStream()); //Messages to the client
 					} catch (IOException e2) {
 						// TODO Auto-generated catch block
 						e2.printStackTrace();
 					}
 
-					// read the data
-					String recMsg = null;
+					String recMsg = "";
 
 					try {
-						recMsg = fromClient.readLine().toString();
+						recMsg = fromClient.readLine(); //Gets the message from the client
 					} catch (IOException e1) {
 						// TODO Auto-generated catch block
 						e1.printStackTrace();
@@ -63,45 +63,45 @@ public class Server implements Runnable {
 
 					int firstSpace = recMsg.indexOf(" ");
 										
-					String status = recMsg.substring(0, firstSpace).trim();
+					String status = recMsg.substring(0, firstSpace).trim(); //Gets the status from the client's message
 					
 					String groupName = "";
 					
 					try {
-						if(status.equalsIgnoreCase("post")){
-							groupName = recMsg.substring(firstSpace).trim();
-							for (int i = 0; i < groupName.length(); i++) {
-								if (Character.isISOControl(groupName.charAt(i))) {
+						if(status.equalsIgnoreCase("post")){ //If the client's status is "post"
+							groupName = recMsg.substring(firstSpace).trim(); //Gets the group name from the client's message
+							for (int i = 0; i < groupName.length(); i++) { //Iterates through the client's group name
+								if (Character.isISOControl(groupName.charAt(i))) { //If client's group name contains a control character
 									try {
-										toClient.writeBytes("error: invalid group name");
-										System.exit(1);
+										toClient.writeBytes("error: invalid group name"); //Send error message to client
+										System.exit(1); //Exit the thread
 									} catch (IOException e) {
 										// TODO Auto-generated catch block
 										e.printStackTrace();
 									}
 								}
 							}
-							runPost(conn, groupName, fromClient, toClient);
+							runPost(conn, groupName, fromClient, toClient); //Continue talking with client
 						}
-						else if (status.equalsIgnoreCase("get")){
-							groupName = recMsg.substring(firstSpace).trim();
-							for (int i = 0; i < groupName.length(); i++) {
-								if (Character.isISOControl(groupName.charAt(i))) {
+						else if (status.equalsIgnoreCase("get")){ //If the client's status is "post"
+							groupName = recMsg.substring(firstSpace).trim(); //Gets the group name from the client's message
+							for (int i = 0; i < groupName.length(); i++) { //Iterates through the client's group name
+								if (Character.isISOControl(groupName.charAt(i))) { //If the client's group name contains a control character
 									try {
-										toClient.writeBytes("error: invalid group name");
-										System.exit(1);
+										toClient.writeBytes("error: invalid group name"); //Send error message to client
+										System.exit(1); //Exit the thread
 									} catch (IOException e) {
 										// TODO Auto-generated catch block
 										e.printStackTrace();
 									}
 								}
 							}
-							runGet(conn, groupName, fromClient, toClient);
+							runGet(conn, groupName, fromClient, toClient); //Continue talking with client
 						}
-					else {
+					else { //If the client's status is neither "post" nor "get"
 						try {
-							toClient.writeBytes("error: invalid command");
-							System.exit(1);
+							toClient.writeBytes("error: invalid command"); //Send error message to client
+							System.exit(1); //Exit the thread
 						} catch (IOException e) {
 							// TODO Auto-generated catch block
 							e.printStackTrace();
@@ -111,53 +111,52 @@ public class Server implements Runnable {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
-					ArrayList<String> clientRequest = new ArrayList<String>(); //UserRequests are <= 6, so you can just base it off size
-					clientRequest.add(status); //add all tokens here to store the entire request
 
 				}});
-			t.start();
+			t.start(); //Starts the thread
 		}
 	}
 
+	//Server communicating with get client
 	private static void runGet(Socket conn, String groupName, BufferedReader fromClient, DataOutputStream toClient) throws IOException {
-		if (serv.containsKey(groupName)) {
-			toClient.writeBytes("ok\n");
-			toClient.writeBytes(serv.get(groupName).size() + " message(s)\n");
-			for(message mess: serv.get(groupName)){
-				toClient.writeBytes(mess.toString());
+		if (serv.containsKey(groupName)) { //If the requested group name is in the hash map
+			toClient.writeBytes("ok\n"); //Tell the client "ok"
+			toClient.writeBytes(serv.get(groupName).size() + " message(s)\n"); //Tell the client how many messages are in the group
+			for(message mess: serv.get(groupName)){ //For each message in the group 
+				toClient.writeBytes(mess.toString()); //Send each message to the client
 			}
-			conn.close();
+			conn.close(); //Ends connection with client
 			return;
-		} else {
-			toClient.writeBytes("error: invalid group name");
-			conn.close();
+		} else { //If the requested group name is not in the hash map
+			toClient.writeBytes("error: invalid group name"); //Sends error message to the client
+			conn.close(); //Ends connection with client
 			return;
 		}
 	}
 
+	//Server communicating with post client
 	private static void runPost(Socket sock, String groupName, BufferedReader fromClient, DataOutputStream toClient) throws IOException{
-		//CHECK FOR PROPER INPUTS - MIKHAIL 
-		toClient.writeBytes("ok\n");
-		String username = fromClient.readLine();
-		toClient.writeBytes("ok\n");
-		String clientMsg = "";                
+		toClient.writeBytes("ok\n"); //Tells the client "ok"
+		String username = fromClient.readLine(); //Gets username from client
+		toClient.writeBytes("ok\n"); //Tells the client "ok"
+		String clientMsg = "";
         String add = "";
 
-        while((add = fromClient.readLine()) != null){
-        	clientMsg = clientMsg + add + "\n";
+        while((add = fromClient.readLine()) != null){ //While the client is continuing to write their message
+        	clientMsg = clientMsg + add + "\n"; //Update their message
         }		
 
-		if (serv.containsKey(groupName)){
-			List<message> temp = serv.get(groupName);
-			temp.add(new message(username, clientMsg, sock.getRemoteSocketAddress().toString()));
+		if (serv.containsKey(groupName)){ //If the requested group name is in the hash map
+			List<message> temp = serv.get(groupName); //Get the corresponding list of messages
+			temp.add(new message(username, clientMsg, sock.getRemoteSocketAddress().toString())); //Add the client's message to the list
 		}
-		else {
-			List<message> temp = new ArrayList<message>();
-			temp.add(new message(username, clientMsg, sock.getRemoteSocketAddress().toString()));
-			serv.put(groupName, temp);
+		else { //If the requested group name is not in the hash map
+			List<message> temp = new ArrayList<message>(); //Create a new group
+			temp.add(new message(username, clientMsg, sock.getRemoteSocketAddress().toString())); //Add the client's message to the group
+			serv.put(groupName, temp); //Add the group to the hash map
 		}
 
-		sock.close();
+		sock.close(); //Ends connection with client
 	}
 	
 	@Override
